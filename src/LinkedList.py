2d7c85733d389236aa1d9fe1259bd394269b6419
DaxGute <daxton@gutekunst.com>
2021-11-12 09:27:09 -0800

    programmed in test cases

--- src/LinkedList.py
+++ src/LinkedList.py
@@ -26,31 +26,39 @@ class LinkedList:
         n = Node(item)
         if self.size == 0:
             self.head = n
+            self.tail = n
         else:
             self.tail.setNext(n)
             self.tail = n
-            self.size += 1
+        self.size += 1
 
     def prepend(self, item):
         """ add new node, containing item, to beginning of the list """
         n = Node(item)
         if self.size == 0:
             self.head = n
+            self.tail = n
         else:
             n.setNext(self.head)
             self.head = n
-            self.size += 1
+        self.size += 1
 
     def deleteHead(self):
+        self.size -= 1
+        prevHead = self.head
         self.head = self.head.getNext()
+        return prevHead.getItem()
     
     def deleteTail(self):
         newTail = self.head
         for i in range(self.size - 1):
-            newTail = newTail.getNext
+            newTail = newTail.getNext()
         
+        prevTail = self.tail
         self.tail = newTail
-        self.tail.getNext(None)
+        self.tail.getNext()
+        self.size -= 1
+        return prevTail.getItem()
 
     def count(self, item):
         numNodes = 0
@@ -75,15 +83,15 @@ class LinkedList:
     def insert(self, index, item):
         n = Node(item)
         if (index == 0):
-            self.prepend(index)
-        elif (index == self.size - 1):
-            self.append(index)
+            self.prepend(item)
+        elif (index == self.size):
+            self.append(item)
         else:
             leftNode = None
             rightNode = None
 
             curr = self.head
-            for i in range(index):
+            for i in range(index-1):
                 curr = curr.getNext()
 
             leftNode = curr
@@ -91,11 +99,12 @@ class LinkedList:
 
             leftNode.setNext(n)
             n.setNext(rightNode)  
+            self.size += 1
 
     def pop(self, index):
         if (index == 0):
             self.deleteHead()
-        elif (index == self.size-1):
+        elif (index == self.size):
             self.deleteTail()
         else:
             leftNode = self.head
@@ -104,6 +113,7 @@ class LinkedList:
 
             rightNode = leftNode.getNext().getNext()
             leftNode.setNext(rightNode)
+            self.size -= 1
 
     def remove(self, item):
         index = self.index(item)
@@ -119,5 +129,39 @@ if __name__ == "__main__":		   # test the following methods: init, str, append, 
     LL.append("B")
     LL.append("C")
     assert len(LL) == 3
-    assert str(LL) == "head→(A)(B)(C)<--tail"
+    assert str(LL) == "head-->(A)(B)(C)<--tail"
+    
+    LL.prepend("A")
+    LL.prepend("B")
+    LL.prepend("C")
+    assert len(LL) == 6
+    assert str(LL) == "head-->(C)(B)(A)(A)(B)(C)<--tail"
+
+    LL.deleteHead()
+    assert len(LL) == 5
+    assert str(LL) == "head-->(B)(A)(A)(B)(C)<--tail"
+
+    LL.deleteTail()
+    assert len(LL) == 4
+    assert str(LL) == "head-->(B)(A)(A)(B)<--tail"
+
+
+    assert LL.count("A") == 2
+    assert LL.count("B") == 2
+
+    assert LL.index("A") == 1
+    assert LL.index("B") == 0
+
+    LL.insert(2, "C")
+    assert str(LL) == "head-->(B)(A)(C)(A)(B)<--tail"
+
+    LL.pop(0)
+    LL.pop(0)
+    assert str(LL) == "head-->(C)(A)(B)<--tail"
+
+    LL.remove("A")
+    assert str(LL) == "head-->(C)(B)<--tail"
+    
+
+
 
